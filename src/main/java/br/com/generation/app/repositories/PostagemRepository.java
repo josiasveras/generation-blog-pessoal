4722e9c1d26eb3b156309e263e7d4f1838a50181
josiasveras <josias_veras@hotmail.com>
2022-01-20 19:42:37 -0300

    Criação interface PostagemRepository

---
+++ src/main/java/br/com/generation/app/repositories/PostagemRepository.java
@@ -0,0 +1,13 @@
+package br.com.generation.app.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.generation.app.models.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+}
