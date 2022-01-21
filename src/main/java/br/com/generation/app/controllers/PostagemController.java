2e227bcbcc40573e2c969fdb3832941bca71373f
josiasveras <josias_veras@hotmail.com>
2022-01-20 21:10:27 -0300

    Criação método buscarTodasPostagens

--- src/main/java/br/com/generation/app/controllers/PostagemController.java
+++ src/main/java/br/com/generation/app/controllers/PostagemController.java
@@ -1,10 +1,15 @@
 package br.com.generation.app.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.generation.app.models.Postagem;
 import br.com.generation.app.repositories.PostagemRepository;
 
 @RestController
@@ -14,4 +19,19 @@ public class PostagemController {
 
 	@Autowired
 	private PostagemRepository postagemRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> buscarTodasPostagens(){
+		List<Postagem> list = postagemRepository.findAll();
+		
+		if (list.isEmpty()) {
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(200).body(list);
+		}
+	}
+	
+	/*public ResponseEntity<List<Postagem>> getAll() {
+		return ResponseEntity.ok(postagemRepository.findAll());
+	}*/
 }
