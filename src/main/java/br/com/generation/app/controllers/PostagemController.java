b4a1466f81c4770a0f0e156bcece07252419f5ec
josiasveras <josias_veras@hotmail.com>
2022-01-22 16:18:41 -0300

    @josiasveras [Task: Criar método salvarPostagem]

--- src/main/java/br/com/generation/app/controllers/PostagemController.java
+++ src/main/java/br/com/generation/app/controllers/PostagemController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -53,5 +55,10 @@ public class PostagemController {
 		} else {
 			return ResponseEntity.status(200).body(list);
 		}
+	}
+	
+	@PostMapping("/salvar")
+	public ResponseEntity<Postagem> salvarPostagem(@RequestBody Postagem postagem) {
+		return ResponseEntity.status(201).body(postagemRepository.save(postagem));
 	}
 }
