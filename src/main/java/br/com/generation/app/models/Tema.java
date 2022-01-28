bf07597b6452c49c6fb267b75a8a9522e2d61059
josiasveras <josias_veras@hotmail.com>
2022-01-28 14:02:25 -0300

    @josiasveras [Task: Criar model Tema]

---
+++ src/main/java/br/com/generation/app/models/Tema.java
@@ -0,0 +1,56 @@
+package br.com.generation.app.models;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_temas")
+public class Tema {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idTema;
+	
+	@NotBlank
+	@Size(min = 2, max = 100)
+	private String descricaoTema;
+	
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
+
+	public long getIdTema() {
+		return idTema;
+	}
+
+	public void setIdTema(long idTema) {
+		this.idTema = idTema;
+	}
+
+	public String getDescricaoTema() {
+		return descricaoTema;
+	}
+
+	public void setDescricaoTema(String descricaoTema) {
+		this.descricaoTema = descricaoTema;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+}
