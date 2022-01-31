259ea62fa80351f1a72b6bda49288e472dba7ea7
josiasveras <josias_veras@hotmail.com>
2022-01-31 11:48:40 -0300

    @josiasveras [Task: Criar model UserLogin]

---
+++ src/main/java/br/com/generation/app/models/UserLogin.java
@@ -0,0 +1,34 @@
+package br.com.generation.app.models;
+
+public class UserLogin {
+	
+	private String nome;
+	private String login;
+	private String senha;
+	private String token;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+}
