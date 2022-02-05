4b5c1c8d8da2768130e9dfcffef2d66302c11756
josiasveras <josias_veras@hotmail.com>
2022-02-04 22:43:03 -0300

    @josiasveras [Task: Atualizar UsuarioRepository]

--- src/main/java/br/com/generation/app/repositories/UsuarioRepository.java
+++ src/main/java/br/com/generation/app/repositories/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package br.com.generation.app.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,6 @@ import br.com.generation.app.models.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
 	public Optional<Usuario> findByUsuario(String login);
+	
+	public List <Usuario> findAllByNomeContainingIgnoreCase(String nome);
 }
